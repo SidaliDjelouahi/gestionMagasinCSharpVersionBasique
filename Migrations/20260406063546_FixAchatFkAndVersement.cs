bb52591730578e95edc360590066924c1d6cda3f
sidali <sidalidjelouahi@gmail.com>
2026-04-06 07:36:09 +0100

    Fix Achat FK: use IdFournisseur as FK and add Versement; migration

---
+++ Migrations/20260406063546_FixAchatFkAndVersement.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C_GS.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAchatFkAndVersement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Achats_Fournisseurs_FournisseurId",
+                table: "Achats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Achats_FournisseurId",
+                table: "Achats");
+
+            migrationBuilder.DropColumn(
+                name: "FournisseurId",
+                table: "Achats");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Versement",
+                table: "Achats",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achats_IdFournisseur",
+                table: "Achats",
+                column: "IdFournisseur");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Achats_Fournisseurs_IdFournisseur",
+                table: "Achats",
+                column: "IdFournisseur",
+                principalTable: "Fournisseurs",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Achats_Fournisseurs_IdFournisseur",
+                table: "Achats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Achats_IdFournisseur",
+                table: "Achats");
+
+            migrationBuilder.DropColumn(
+                name: "Versement",
+                table: "Achats");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FournisseurId",
+                table: "Achats",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achats_FournisseurId",
+                table: "Achats",
+                column: "FournisseurId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Achats_Fournisseurs_FournisseurId",
+                table: "Achats",
+                column: "FournisseurId",
+                principalTable: "Fournisseurs",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/Achat.cs
+++ Models/Achat.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MonAppGestion.Models
 {
@@ -9,9 +10,14 @@ namespace MonAppGestion.Models
         public string NumAchat { get; set; } = string.Empty;
         public DateTime Date { get; set; }
 
-        // reference to fournisseur (nullable)
+        // reference to fournisseur (nullable) - use IdFournisseur as the FK column
         public int? IdFournisseur { get; set; }
+
+        [ForeignKey("IdFournisseur")]
         public Fournisseur? Fournisseur { get; set; }
+
+        // amount paid for the purchase
+        public decimal Versement { get; set; } = 0m;
 
         public List<AchatDetail> Details { get; set; } = new List<AchatDetail>();
     }
