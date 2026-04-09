00125469586dd1fc55f6fc47b8e95e3f755cf564
sidali <sidalidjelouahi@gmail.com>
2026-04-09 15:32:11 +0100

    WIP: sauvegarde avant migration

---
+++ Migrations/20260409080531_AddRaccourciToProduct.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonAppGestion.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRaccourciToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
